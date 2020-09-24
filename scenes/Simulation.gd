74a9a2e83cdb6835652f3c6ab6205241db5d6a81
edearth <nepormoseno1992@gmail.com>
2020-09-24 23:09:01 +0200

    Cleanup code

--- scenes/Simulation.gd
+++ scenes/Simulation.gd
@@ -21,15 +21,13 @@ func run(_parameters):
 	while true:
 		tick()
 		physics_tick()
-
-func tick():
-	print("Start simulation tick")
-	for actor in actors.get_children():
-		actor.tick()
 	print("End simulation tick")
 
+func tick():
+	for actor in actors.get_children():
+		actor.tick()
+
 func physics_tick():
-	print("Start simulation physics tick")
 	for actor in get_tree().get_nodes_in_group(Groups.PHYSICS):
 		move_actor(actor)
 		
@@ -39,8 +37,6 @@ func physics_tick():
 		elif not actor.is_falling:
 			actor.velocity.x = 0
 			actor.is_falling = true
-		
-	print("End simulation physics tick")
 
 func move_actor(actor):
 	var final_coords = Vector2(actor.coordinates.x + actor.velocity.x, actor.coordinates.y + actor.velocity.y)
@@ -55,15 +51,9 @@ func step_through_movement(initial_coords, final_coords):
 		var next_coords = current_coords
 		
 		if is_vertical_distance_bigger_than_horizontal(initial_coords, final_coords, current_coords):
-			if current_coords.x > final_coords.x:
-				next_coords.x -= 1
-			else:
-				next_coords.x += 1
+			next_coords.x += normalize(final_coords.x - next_coords.x)
 		else:
-			if current_coords.y > final_coords.y:
-				next_coords.y -= 1
-			else:
-				next_coords.y += 1
+			next_coords.y += normalize(final_coords.y - next_coords.y)
 			
 		if is_cell_ground(next_coords):
 			break
@@ -71,11 +61,12 @@ func step_through_movement(initial_coords, final_coords):
 			current_coords = next_coords
 	return current_coords
 
+func normalize(val :float):
+	return val / abs(val)
+
 func is_vertical_distance_bigger_than_horizontal(initial_coords, final_coords, current_coords):
-	var y_completion : int = 1 if initial_coords.y == final_coords.y else abs(inverse_lerp(initial_coords.y, final_coords.y, current_coords.y))
-	var x_completion : int = 1 if initial_coords.x == final_coords.x else abs(inverse_lerp(initial_coords.x, final_coords.x, current_coords.x))
-	print("!!! > y ilerp: "+str(y_completion))
-	print("!!! > x ilerp: "+str(x_completion))
+	var y_completion : int = 1 if initial_coords.y == final_coords.y else int(abs(inverse_lerp(initial_coords.y, final_coords.y, current_coords.y)))
+	var x_completion : int = 1 if initial_coords.x == final_coords.x else int(abs(inverse_lerp(initial_coords.x, final_coords.x, current_coords.x)))
 	return y_completion if y_completion is int else 1 >= x_completion if x_completion is int else 1
 
 func hit_ground(actor):
--- scenes/components/Controller/PlayerKeyboardController.gd
+++ scenes/components/Controller/PlayerKeyboardController.gd
@@ -8,22 +8,10 @@ func _ready():
 	EventBus.connect("player_finished_input", self, "player_finished_input")
 
 func tick():
-	print("Wait for player input")
 	input_press.wait()
-	print("Player input selected")
 
 func player_finished_input():
 	input_press.post()
 
 func _process(delta):
 	state_machine._process(delta)
-	
-	#	elif Input.is_action_just_pressed("grab"):
-	#		print("! Grab where?")
-	#		selectingGrab = true
-	#		return
-	#	elif Input.is_action_just_pressed("drop"):
-	#		player.drop()
-	#		input_press.post()
-
-
