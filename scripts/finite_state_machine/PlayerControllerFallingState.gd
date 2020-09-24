32938dcf46da95c1d2601eab1db5aeb1a193c1ea
edearth <nepormoseno1992@gmail.com>
2020-09-24 18:58:11 +0200

    Use FallingState instead of 'if' in IdleState

--- scripts/finite_state_machine/PlayerControllerFallingState.gd
+++ scripts/finite_state_machine/PlayerControllerFallingState.gd
@@ -1,13 +1,24 @@
 extends Node
 
-var stateMachine
+var player
+var map : TileMap
+var state_machine
+
+var idle_state = "res://scripts/finite_state_machine/PlayerControllerIdleState.gd"
 
 func on_enter(_stateMachine):
-	stateMachine = _stateMachine
-	pass
+	state_machine = _stateMachine
+	map = Singletons.get_instance(Singletons.MAP)
+	player = Singletons.get_instance(Singletons.PLAYER)
 
 func on_exit():
 	pass
 
-func _process(delta):
-	pass
+func _process(_delta):
+	if not player.is_falling:
+		print("! You land.")
+		state_machine.change_state(load(idle_state).new())
+
+	if Input.is_action_just_pressed("wait"):
+		print("! You wait.")
+		EventBus.emit_signal("player_finished_input")
--- scripts/finite_state_machine/PlayerControllerIdleState.gd
+++ scripts/finite_state_machine/PlayerControllerIdleState.gd
@@ -4,7 +4,9 @@ class_name PlayerControllerIdleState
 var player
 var map : TileMap
 var state_machine
+
 var jump_state = "res://scripts/finite_state_machine/PlayerControllerSelectingJumpState.gd"
+var falling_state = "res://scripts/finite_state_machine/PlayerControllerFallingState.gd"
 
 func on_enter(_stateMachine):
 	state_machine = _stateMachine
@@ -14,7 +16,10 @@ func on_enter(_stateMachine):
 func on_exit():
 	pass
 
-func _process(delta):
+func _process(_delta):
+	if player.is_falling:
+		state_machine.change_state(load(falling_state).new())
+
 	if Input.is_action_just_pressed("wait"):
 		print("! You wait.")
 		EventBus.emit_signal("player_finished_input")
@@ -24,21 +29,20 @@ func _process(delta):
 #	elif Input.is_action_just_pressed("down"):
 #		player.set_movement(Vector2.DOWN)
 #		input_press.post()
-	elif not player.is_falling:
-		if Input.is_action_just_pressed("left"):
-			var vel = player.get_velocity()
-			vel.x = -1
-			if is_cell_free(player.coordinates + vel):
-				player.set_velocity(vel)
-				EventBus.emit_signal("player_finished_input")
-		elif Input.is_action_just_pressed("right"):
-			var vel = player.get_velocity()
-			vel.x = 1
-			if is_cell_free(player.coordinates + vel):
-				player.set_velocity(vel)
+	elif Input.is_action_just_pressed("left"):
+		var vel = player.get_velocity()
+		vel.x = -1
+		if is_cell_free(player.coordinates + vel):
+			player.set_velocity(vel)
 			EventBus.emit_signal("player_finished_input")
-		elif Input.is_action_just_pressed("jump"):
-			state_machine.change_state(load(jump_state).new())
+	elif Input.is_action_just_pressed("right"):
+		var vel = player.get_velocity()
+		vel.x = 1
+		if is_cell_free(player.coordinates + vel):
+			player.set_velocity(vel)
+		EventBus.emit_signal("player_finished_input")
+	elif Input.is_action_just_pressed("jump"):
+		state_machine.change_state(load(jump_state).new())
 
 func is_cell_free(coord):
 	return map.get_cell(coord.x, coord.y) == TileMap.INVALID_CELL
